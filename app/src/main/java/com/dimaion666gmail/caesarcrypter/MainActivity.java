5e49dddf701e044b7156640e5d167fb3f42250d0
Dmitry <dimaion666@gmail.com>
2020-12-14 14:37:58 +0500

    Add buttons methods

--- app/src/main/java/com/dimaion666gmail/caesarcrypter/MainActivity.java
+++ app/src/main/java/com/dimaion666gmail/caesarcrypter/MainActivity.java
@@ -2,6 +2,7 @@ package com.dimaion666gmail.caesarcrypter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickEncryptOrDecrypt(View view) {
+
+    }
+
+    public void onClickTranslate(View view) {
+
     }
 }
