85e2d360462c40fd8a1436feff2eba044e682e72
Dmitry <dimaion666@gmail.com>
2020-12-15 12:48:15 +0500

    Define encrypting or decrypting letter shifts

--- app/src/main/java/com/dimaion666gmail/caesarcrypter/CaesarCrypter.java
+++ app/src/main/java/com/dimaion666gmail/caesarcrypter/CaesarCrypter.java
@@ -19,9 +19,13 @@ public class CaesarCrypter {
         for (int i = 0; i < letterShiftsString.length; i++) {
             letterShiftsInt[i] = Integer.parseInt(letterShiftsString[i]);
         }
-        // Define encrypting or decrypting letter shift
+        // Define encrypting or decrypting letter shifts
         if (encryptOrDecrypt == 0) {
-            
+            for (int i = 0; i < letterShiftsInt.length; i++)
+                letterShiftsInt[i] = Math.abs(letterShiftsInt[i]);
+        } else {
+            for (int i = 0; i < letterShiftsInt.length; i++)
+                letterShiftsInt[i] = letterShiftsInt[i] * (-1);
         }
         // Translate text
         return null;
