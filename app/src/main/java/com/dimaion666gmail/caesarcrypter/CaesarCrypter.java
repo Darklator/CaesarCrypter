ba641be72f6de4869c6005f2448da407318f5ac7
Dmitry <dimaion666@gmail.com>
2020-12-15 12:43:52 +0500

    Turn letterShift into an array of shifts
    String letterShift becomes integer array letterShifts

--- app/src/main/java/com/dimaion666gmail/caesarcrypter/CaesarCrypter.java
+++ app/src/main/java/com/dimaion666gmail/caesarcrypter/CaesarCrypter.java
@@ -3,7 +3,7 @@ package com.dimaion666gmail.caesarcrypter;
 public class CaesarCrypter {
     // "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" "abcdefghijklmnopqrstuvwxyz"
     private char[][] alphabet = new char[2][];
-    private int[] letterShifts;
+    private int[] letterShiftsInt;
 
 
     public CaesarCrypter() {
@@ -14,12 +14,14 @@ public class CaesarCrypter {
     public String translate(int encryptOrDecrypt, String letterShift, String text) {
         // Turn letterShift into an array of shifts
         String[] letterShiftsString = letterShift.split(" ");
+        letterShiftsInt = new int[letterShiftsString.length];
+
         for (int i = 0; i < letterShiftsString.length; i++) {
-            
+            letterShiftsInt[i] = Integer.parseInt(letterShiftsString[i]);
         }
         // Define encrypting or decrypting letter shift
         if (encryptOrDecrypt == 0) {
-
+            
         }
         // Translate text
         return null;
