3bbb579bcb631003effdb8e95131c533daec8cc7
Dmitry <dimaion666@gmail.com>
2020-12-15 12:37:16 +0500

    Start coding CaesarCrypter

--- app/src/main/java/com/dimaion666gmail/caesarcrypter/CaesarCrypter.java
+++ app/src/main/java/com/dimaion666gmail/caesarcrypter/CaesarCrypter.java
@@ -3,6 +3,7 @@ package com.dimaion666gmail.caesarcrypter;
 public class CaesarCrypter {
     // "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" "abcdefghijklmnopqrstuvwxyz"
     private char[][] alphabet = new char[2][];
+    private int[] letterShifts;
 
 
     public CaesarCrypter() {
@@ -11,6 +12,16 @@ public class CaesarCrypter {
     }
 
     public String translate(int encryptOrDecrypt, String letterShift, String text) {
+        // Turn letterShift into an array of shifts
+        String[] letterShiftsString = letterShift.split(" ");
+        for (int i = 0; i < letterShiftsString.length; i++) {
+            
+        }
+        // Define encrypting or decrypting letter shift
+        if (encryptOrDecrypt == 0) {
+
+        }
+        // Translate text
         return null;
     }
 }
