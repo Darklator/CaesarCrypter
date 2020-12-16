6cf7c97c5b83b362b517eab3d1d882564650ab8c
Dmitry <dimaion666@gmail.com>
2020-12-16 12:59:57 +0500

    Update CaesarCrypterUnitTest.java

--- app/src/test/java/com/dimaion666gmail/caesarcrypter/CaesarCrypterUnitTest.java
+++ app/src/test/java/com/dimaion666gmail/caesarcrypter/CaesarCrypterUnitTest.java
@@ -25,6 +25,7 @@ public class CaesarCrypterUnitTest {
         assertEquals("ЯАБ", caesarCrypter.translate(true,"1", "АБВ"));
         assertEquals("BCD", caesarCrypter.translate(false,"1", "ABC"));
         assertEquals("ZAB", caesarCrypter.translate(true,"1", "ABC"));
+        assertEquals("ABC", caesarCrypter.translate(true,"", "ABC"));
         // TODO: I should make it possible to input negative key memebers
     }
 }
