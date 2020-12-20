2c4a5bf9ecff457e7b511fc1dc09f1c99fc8b094
Dmitry <dimaion666@gmail.com>
2020-12-20 14:17:32 +0500

    Deleting useless imports

--- app/src/main/java/com/dimaion666gmail/caesarcrypter/MainActivity.java
+++ app/src/main/java/com/dimaion666gmail/caesarcrypter/MainActivity.java
@@ -2,13 +2,8 @@ package com.dimaion666gmail.caesarcrypter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
