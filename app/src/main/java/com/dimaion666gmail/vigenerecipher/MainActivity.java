c81cd594c6faf80a4a939cb42a2c815e6f78a20d
Dmitry <dimaion666@gmail.com>
2020-12-31 10:35:37 +0500

    Update MainActivity.java
    Useless comments deletion

--- app/src/main/java/com/dimaion666gmail/vigenerecipher/MainActivity.java
+++ app/src/main/java/com/dimaion666gmail/vigenerecipher/MainActivity.java
@@ -20,6 +20,8 @@ import static android.content.Intent.ACTION_SEND;
 public class MainActivity extends AppCompatActivity {
     // TODO: User paste string must not save its previous font
 
+    // Эти переменные используются в нескольких местах, поэтому я решил сделать их глобальными. Так
+    // используется меньше кода.
     private ToggleButton isDecryptingToggleButton;
     private EditText keyEditTextView;
     private EditText textToBeTranslatedEditTextView;
@@ -63,16 +65,15 @@ public class MainActivity extends AppCompatActivity {
         textToBeTranslatedEditTextView = (EditText) findViewById(R.id.text_to_be_translated_text);
         translatedTextTextView = (TextView) findViewById(R.id.translated_text_text);
 
-
         Intent intent = getIntent();
         String action = intent.getAction();
 
-        // Если приложение было вызвано через ACTION_SEND, то получаем текст, который хочет перевести пользователь
+        // Если активность была вызвана через ACTION_SEND, то получаем текст, который хочет перевести пользователь
         if (ACTION_SEND.equals(action))
             textToBeTranslatedEditTextView.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
 
         if (savedInstanceState != null) {
-            // Сохраняется только translatedText, потому что её представление сбрасывает содержимое,
+            // Сохраняется только translatedText, потому что только её представление сбрасывает содержимое,
             // а translatedText обнуляется.
             translatedText = savedInstanceState.getString("translatedText");
             translatedTextTextView.setText(translatedText);
@@ -85,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         // Сохраняется только переменная translatedText, так как содержимое других представлений
         // не сбрасывается со сменой конфигурации устройства. Скорее всего, такая особенность появилась
         // из-за использования шаблона карточки через заполнение ViewStub.
-        savedInstanceState.putString("translatedText", translatedText); // Нужно сохранять
+        savedInstanceState.putString("translatedText", translatedText);
         super.onSaveInstanceState(savedInstanceState);
     }
 
@@ -97,10 +98,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 boolean isDecrypting = isDecryptingToggleButton.isChecked();
-                String key = String.valueOf(keyEditTextView.getText()); // key используется только тут, можно глобально удалить
-                String textToBeTranslated = String.valueOf(textToBeTranslatedEditTextView.getText()); // textToBeTranslated  тоже используется только тут, в принципе. Вне этого места  можно удалить.
+                String key = String.valueOf(keyEditTextView.getText());
+                String textToBeTranslated = String.valueOf(textToBeTranslatedEditTextView.getText());
                 try {
-                    translatedText = VigenereCipher.translate(isDecrypting, key, textToBeTranslated); // Лучше оставить глобально
+                    translatedText = VigenereCipher.translate(isDecrypting, key, textToBeTranslated);
                     translatedTextTextView.setText(translatedText);
                 }
                 catch (InvalidKeyException ikex) {
@@ -117,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         if (clipboard.hasPrimaryClip()) {
             ClipData clipData = clipboard.getPrimaryClip();
-            String textToBeTranslated = clipData.getItemAt(0).coerceToText(this).toString(); // Глобальное не нужно
+            String textToBeTranslated = clipData.getItemAt(0).coerceToText(this).toString();
             textToBeTranslatedEditTextView.setText(textToBeTranslated);
         } else {
             Toast toast = Toast.makeText(getApplicationContext(), R.string.no_content, Toast.LENGTH_SHORT);
@@ -131,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickCopy(View view) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-        ClipData clipData = ClipData.newPlainText("Translated text", translatedText); // Поберечь
+        ClipData clipData = ClipData.newPlainText("Translated text", translatedText);
         clipboard.setPrimaryClip(clipData);
         Toast message = Toast.makeText(getApplicationContext(), R.string.output_copied, Toast.LENGTH_SHORT);
         message.show();
@@ -140,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClickShare(View view) {
         Intent intent = new Intent(ACTION_SEND);
         intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, translatedText); // Поберечь
+        intent.putExtra(Intent.EXTRA_TEXT, translatedText);
         startActivity(intent);
     }
 }
