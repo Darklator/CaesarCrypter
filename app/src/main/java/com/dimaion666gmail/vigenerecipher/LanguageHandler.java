4b3874ad83a5fb32cfd08016f9891a7b146dfb1a
Dmitry <dimaion666@gmail.com>
2021-01-08 12:55:02 +0500

    Update LanguageHandler.java

--- app/src/main/java/com/dimaion666gmail/vigenerecipher/LanguageHandler.java
+++ app/src/main/java/com/dimaion666gmail/vigenerecipher/LanguageHandler.java
@@ -8,6 +8,8 @@ package com.dimaion666gmail.vigenerecipher;
  */
 public abstract class LanguageHandler {
     public abstract boolean doesTheLetterExistHere(char letter);
+
     public abstract int getOrderInAlphabet(char letter);
+
     public abstract char shiftLetter(int shiftStep, char letter);
 }
