2a8649c8ec703c13580a92c1b38bc13aa2f33332
Dmitry <dimaion666@gmail.com>
2021-01-08 12:53:42 +0500

    Update LanguageHandler.java

--- app/src/main/java/com/dimaion666gmail/vigenerecipher/LanguageHandler.java
+++ app/src/main/java/com/dimaion666gmail/vigenerecipher/LanguageHandler.java
@@ -1,5 +1,11 @@
 package com.dimaion666gmail.vigenerecipher;
 
+/**
+ * This class contains methods that must be implemented by new language alphabet handler
+ *
+ * @version 1.0 08 Jan 2021
+ * @author Dmitry Ionov
+ */
 public abstract class LanguageHandler {
     public abstract boolean doesTheLetterExistHere(char letter);
     public abstract int getOrderInAlphabet(char letter);
