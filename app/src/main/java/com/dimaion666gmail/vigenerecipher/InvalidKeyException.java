8a859bdefab372903b38c845e2f4934b89a01e00
Dmitry <dimaion666@gmail.com>
2021-01-08 14:19:16 +0500

    Update InvalidKeyException.java

--- app/src/main/java/com/dimaion666gmail/vigenerecipher/InvalidKeyException.java
+++ app/src/main/java/com/dimaion666gmail/vigenerecipher/InvalidKeyException.java
@@ -1,5 +1,11 @@
 package com.dimaion666gmail.vigenerecipher;
 
+/**
+ * This class is custom exception.
+ *
+ * @version 1.0 08 Jan 2021
+ * @author Dmitry Ionov
+ */
 public class InvalidKeyException extends Exception {
     public InvalidKeyException() {
         super("This key contains invalid characters!");
